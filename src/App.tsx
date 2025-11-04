93087ce6dc31b849cc67f48156c9110baa2f63dc
shagar619 <ashagar619@gmail.com>
2025-11-04 23:43:22 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+
+  return (
+    <>
+
+      <h1>Vite + React</h1>
+      <Button>Just click it!</Button>
+
+    </>
+  )
+}
+
+export default App
