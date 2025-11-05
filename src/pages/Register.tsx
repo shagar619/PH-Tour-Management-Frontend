e07259fe8f3da7658343ffd551566fec76536c39
shagar619 <ashagar619@gmail.com>
2025-11-05 22:46:42 +0600

    added register form

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,6 +1,7 @@
 import TravelRegister from "@/assets/images/travel-register.jpg";
 import { Link } from "react-router";
 import Logo from "@/assets/icons/Logo";
+import { RegisterForm } from "@/components/modules/Authentication/RegisterForm";
 
 
 export default function Register() {
@@ -21,7 +22,7 @@ return (
      </div>
      <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">
-               {/* <RegisterForm /> */}
+               <RegisterForm />
           </div>
      </div>
      </div>
