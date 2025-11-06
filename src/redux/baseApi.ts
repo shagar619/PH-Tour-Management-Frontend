2a471fe07f224fcc455dd92bb52b735ab6ab5430
shagar619 <ashagar619@gmail.com>
2025-11-06 09:47:55 +0600

    Configuring Axios and Creating axiosBaseQuery

--- src/redux/baseApi.ts
+++ src/redux/baseApi.ts
@@ -1,6 +1,7 @@
 import { createApi } from "@reduxjs/toolkit/query/react";
 import axiosBaseQuery from "./axiosBaseQuery";
 
+
 export const baseApi = createApi({
      reducerPath: "baseApi",
      baseQuery: axiosBaseQuery(),
