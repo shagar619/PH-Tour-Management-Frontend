f35c2d58c14af800b3f1c55a572405ef5d2fe58c
shagar619 <ashagar619@gmail.com>
2025-11-08 23:49:03 +0600

    added admin add tour type page

---
+++ src/pages/Admin/AddDivision.tsx
@@ -0,0 +1,9 @@
+
+
+export default function AddDivision() {
+     return (
+     <div>
+          <h1> This is AddDivision component </h1>
+     </div>
+);
+}
---
+++ src/pages/Admin/AddTourType.tsx
@@ -0,0 +1,11 @@
+
+
+const AddTourType = () => {
+     return (
+          <div>
+               <h1>This is add tour type page</h1>
+          </div>
+     );
+};
+
+export default AddTourType;
